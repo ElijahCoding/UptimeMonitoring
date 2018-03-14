3bfca9c4c2aa2fe477940c4869a84f538146313d
Elijah <elijahsharper1@gmail.com>
2018-03-14 18:51:56 +0300

    Option value in console

--- app/Console/Commands/AddEndpointCommand.php
+++ app/Console/Commands/AddEndpointCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -11,7 +13,15 @@ class AddEndpointCommand extends Command
   protected function configure()
   {
     $this->setName('endpoint:add')
-         ->setDescription('Add an endpoint to monitor');
+         ->setDescription('Add an endpoint to monitor')
+         ->addArgument('endpoint', InputArgument::REQUIRED, 'The endpoint to monitor')
+         ->addOption(
+           'frequency',
+           'f',
+           InputOption::VALUE_OPTIONAL,
+           'The frequency to check this endpoint, in minutes.',
+           1
+         );
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
